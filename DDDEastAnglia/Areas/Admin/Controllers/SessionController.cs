f19d5cd57166d6255646618cc0736fce09e73c8e
Adrian Banks <github@adrianbanks.co.uk>
2014-06-24 22:37:16 +0100

    *temporary* some stuff

--- DDDEastAnglia/Areas/Admin/Controllers/SessionController.cs
+++ DDDEastAnglia/Areas/Admin/Controllers/SessionController.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using DDDEastAnglia.Areas.Admin.Models;
 using DDDEastAnglia.DataAccess;
+using DDDEastAnglia.Domain.Calendar;
+using DDDEastAnglia.Helpers;
 using DDDEastAnglia.Models;
 
 namespace DDDEastAnglia.Areas.Admin.Controllers
@@ -11,8 +15,10 @@ namespace DDDEastAnglia.Areas.Admin.Controllers
     {
         private readonly ISessionRepository sessionRepository;
         private readonly IVoteRepository voteRepository;
+        private readonly SessionStatsThing sessionStatsThing;
 
-        public SessionController(ISessionRepository sessionRepository, IVoteRepository voteRepository)
+
+        public SessionController(ISessionRepository sessionRepository, IVoteRepository voteRepository, SessionStatsThing sessionStatsThing)
         {
             if (sessionRepository == null)
             {
@@ -24,8 +30,14 @@ namespace DDDEastAnglia.Areas.Admin.Controllers
                 throw new ArgumentNullException("voteRepository");
             }
             
+            if (sessionStatsThing == null)
+            {
+                throw new ArgumentNullException("sessionStatsThing");
+            }
+            
             this.sessionRepository = sessionRepository;
             this.voteRepository = voteRepository;
+            this.sessionStatsThing = sessionStatsThing;
         }
 
         public ActionResult Index()
@@ -81,6 +93,110 @@ namespace DDDEastAnglia.Areas.Admin.Controllers
         {
             sessionRepository.DeleteSession(id);
             return RedirectToAction("Index");
+        }
+
+        public ActionResult SubmissionsStats()
+        {
+            var stats = sessionStatsThing.GetStats();
+            return View(stats);
+        }
+
+        public class SessionStatsThing
+        {
+            private readonly ICalendarItemRepository calendarItemRepository;
+            private readonly ISessionRepository sessionRepository;
+
+            public SessionStatsThing(ICalendarItemRepository calendarItemRepository, ISessionRepository sessionRepository)
+            {
+                if (calendarItemRepository == null)
+                {
+                    throw new ArgumentNullException("calendarItemRepository");
+                }
+
+                if (sessionRepository == null)
+                {
+                    throw new ArgumentNullException("sessionRepository");
+                }
+                
+                this.calendarItemRepository = calendarItemRepository;
+                this.sessionRepository = sessionRepository;
+            }
+
+            public SessionStatsViewModel GetStats()
+            {
+                var dateTimeSubmissionModels = GetData();
+                var submissionsPerDay = ToChartData(dateTimeSubmissionModels);
+                var cumulativeSubmissions = WorkOutCumulativeSubmissions(dateTimeSubmissionModels);
+                return new SessionStatsViewModel
+                {
+                    DayByDay = submissionsPerDay,
+                    Cumulative = cumulativeSubmissions
+                };
+            }
+
+            private IList<DateTimeSubmissionModel> GetData()
+            {
+                var sessionSbumission = calendarItemRepository.GetFromType(CalendarEntryType.SessionSubmission);
+
+                var dateToCountDictionary = sessionRepository.GetAllSessions()
+                                                .Where(s => s.SubmittedAt != null)
+                                                .GroupBy(s => s.SubmittedAt)
+                                                .ToDictionary(g => g.Key, g => g.Count());
+
+                var submissionStartDate = sessionSbumission.StartDate.Date;
+                var submissionEndDate = sessionSbumission.EndDate.Value.Date;
+                var dateTimeSubmissionModels = new List<DateTimeSubmissionModel>();
+
+                for (var day = submissionStartDate.Date; day <= submissionEndDate.Date; day = day.AddDays(1))
+                {
+                    int count;
+                    dateToCountDictionary.TryGetValue(day, out count);
+                    var model = new DateTimeSubmissionModel
+                        {
+                            Date = day,
+                            SubmissionCount = count
+                        };
+                    dateTimeSubmissionModels.Add(model);
+                }
+
+                return dateTimeSubmissionModels;
+            }
+
+            private long[][] WorkOutCumulativeSubmissions(IEnumerable<DateTimeSubmissionModel> votesPerDay)
+            {
+                int totalVotes = 0;
+                var cumulativeVotesPerDay = new List<DateTimeSubmissionModel>();
+
+                foreach (var dateTimeVoteModel in votesPerDay)
+                {
+                    totalVotes += dateTimeVoteModel.SubmissionCount;
+                    var model = new DateTimeSubmissionModel { Date = dateTimeVoteModel.Date, SubmissionCount = totalVotes };
+                    cumulativeVotesPerDay.Add(model);
+                }
+
+                var cumulativeVotesPerDayData = ToChartData(cumulativeVotesPerDay);
+                return cumulativeVotesPerDayData;
+            }
+
+            private long[][] ToChartData(IList<DateTimeSubmissionModel> voteData)
+            {
+                long[][] chartData = new long[voteData.Count][];
+
+                foreach (var item in voteData.Select((v, i) => new { Index = i, Vote = v }))
+                {
+                    var vote = item.Vote;
+                    long javascriptTimestamp = vote.Date.GetJavascriptTimestamp();
+                    chartData[item.Index] = new[] { javascriptTimestamp, vote.SubmissionCount };
+                }
+
+                return chartData;
+            }
+
+            private class DateTimeSubmissionModel
+            {
+                public DateTime Date { get; set; }
+                public int SubmissionCount { get; set; }
+            }
         }
     }
 }
---
+++ DDDEastAnglia/Areas/Admin/Models/SessionStatsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DDDEastAnglia.Areas.Admin.Models
+{
+    public class SessionStatsViewModel
+    {
+        public long[][] DayByDay{get;set;}
+        public long[][] Cumulative{get;set;}
+    }
+}
