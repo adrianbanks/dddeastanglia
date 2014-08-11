b27595542f69db7953609c5d962cb47908fb069d
Adrian Banks <github@adrianbanks.co.uk>
2014-08-11 23:17:35 +0100

    add a controller to return the event details

---
+++ DDDEastAnglia/Controllers/JsonFeed/EventDetailsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Http;
+
+namespace DDDEastAnglia.Controllers.JsonFeed
+{
+    public class EventDetailsController : ApiController
+    {
+        public DDDEvent GetEventDetails()
+        {
+            var dddEvent = new DDDEvent
+            {
+                Id = 1,
+                Version = 5,
+                IsActive = true,
+                Name = "DDD East Anglia 2014",
+                City = "Cambridge",
+                Date = new DateTime(2014, 9, 13, 08, 30, 00)
+            };
+            return dddEvent;
+        }
+    }
+}
