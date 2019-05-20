9fa30d2be95d88fea17590d3188b7dac88e84ab6
Adrian Banks <github@adrianbanks.co.uk>
2019-05-20 21:33:14 +0100

    Add a new columns to conferences for a sessionize id

---
+++ DDDEastAnglia.DatabaseMigrations/Migrations/7-SessionizeSubmissions.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace DDDEastAnglia.DatabaseMigrations.Migrations
+{
+    [Migration(20190610)]
+    public class SessionizeSubmissions : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("Conferences")
+                .AddColumn("SessionizeId").AsString(10).Nullable()
+                .AddColumn("SessionizeName").AsString(50).Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("SessionizeName")
+                .FromTable("Conferences");
+
+            Delete.Column("SessionizeId")
+                .FromTable("Conferences");
+        }
+    }
+}
